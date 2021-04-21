47e401e06f61676f4f643a5dae0df3b4ec6df6d7
Anantha Krishnan <ananthk324@gmail.com>
2021-04-21 20:52:48 +0530

    user auth refactor

---
+++ controllers/auth/user.js
@@ -0,0 +1,28 @@
+const {
+  generateAccessToken,
+  generateRefreshToken,
+} = require("../../utils/generateAccessToken");
+const { getGoogleUser } = require("../../utils/oauth");
+const { CLIENT_BASE_URL } = process.env;
+
+const usersArray = [{ email: "ananthk324@gmail.com" }];
+
+const authenticateUser = async (req, res) => {
+  const code = req.query.code;
+  const user = await getGoogleUser(code);
+
+  console.log(user);
+
+  const __user = usersArray.find(_user => _user.email === user.email);
+
+  if (!__user) return res.redirect(`${CLIENT_BASE_URL}`);
+
+  const accessToken = generateAccessToken(__user.email);
+  const refreshToken = generateRefreshToken(__user.email);
+
+  return res.redirect(
+    `${CLIENT_BASE_URL}/#access_token=${accessToken}&refresh_token=${refreshToken}`
+  );
+};
+
+module.exports = { authenticateUser };
--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const {
-  dataTemplate,
-  serverErrorTemplate,
-} = require("../utils/responseTemplates");
-const { getGoogleAuthURL, getGoogleUser } = require("../utils/oauth");
+const { getGoogleAuthURL } = require("../utils/oauth");
+const { authenticateUser } = require("../controllers/auth/user");
 
-router.get("/login", (req, res) => {
-  console.log("Google login.");
-  res.redirect(getGoogleAuthURL());
-});
+router.get("/login", (req, res) => res.redirect(getGoogleAuthURL()));
 
-router.get("/google/redirect", async (req, res) => {
-  try {
-    const code = req.query.code;
-    console.log(code);
-    const user = await getGoogleUser(code);
-    return dataTemplate(res, user);
-  } catch (err) {
-    console.log(err);
-    return serverErrorTemplate(res, err.message);
-  }
-});
+router.get(
+  "/google/redirect",
+  async (req, res) => await authenticateUser(req, res)
+);
 
 module.exports = router;
